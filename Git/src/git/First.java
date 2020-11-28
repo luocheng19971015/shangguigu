c8ce190ae119e861c7ed8da7c9fe84bda2fa9636
luocheng <864510320@qq.com>
2020-11-28 21:19:52 +0800

    First.java提交

---
+++ Git/src/git/First.java
@@ -0,0 +1,12 @@
+package git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello,SB zhanghaitao");
+        System.out.println("hello,SB zhanghaitao");
+        System.out.println("hello,SB zhanghaitao");
+        System.out.println("hello,SB zhanghaitao");
+        System.out.println("hello,SB zhanghaitao");
+
+    }
+}
